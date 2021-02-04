620394ef6279217c1a00705f07993c397afba99c
ernstt@umsystem.edu <ernstt@umsystem.edu>
2021-02-04 16:56:13 -0600

    Initial Creation of the Post Type

--- index.php
+++ index.php
@@ -7,3 +7,40 @@
  * Author: Tyler Ernst, Rebecca Thomas, Creative Director Monica Pitts
  * Author URI: http://www.mayecreate.com/
  */
+class MC_Event_Post_Types {
+	
+	public static function init() {
+			add_action( 'init', array( __CLASS__, 'register_taxonomies' ), 5 );
+			add_action( 'init', array( __CLASS__, 'mayecreate_create_post_type_event' ), 5 );
+		}
+
+	public static function register_taxonomies() {
+	   register_taxonomy( 'eventcategory', 'menu', array( 'hierarchical' => true, 'label' => 'Event Categories', 'query_var' => true, 'rewrite' => true, 'show_in_rest' => true ) );
+	}
+
+	public static function mayecreate_create_post_type_event() {
+			register_post_type( 'mc_events',
+				array(
+					'labels' => array(
+						'name'              => __( 'Events'),
+						'singular_name'     => __( 'Event' ),
+						'add_new'           => __( 'Add Event' ),
+						'add_new_item'      => __( 'Add New Event' ),
+						'edit_item'         => __( 'Edit Event' ),  
+
+					),
+				'public' => true,
+				'menu_position' => 10,
+				'rewrite' => array('slug' => 'event', 'with_front' => false),
+				'supports' => array('title','thumbnail','revisions','editor'),
+				'menu_icon'         => 'dashicons-calendar',
+				'taxonomies' => array('eventcategory'),
+				'show_in_rest' => true,
+				'has_archive' => true 
+				)
+			);
+	}
+	
+}
+
+MC_Event_Post_types::init();
