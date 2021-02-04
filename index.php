7dd82331553fae19754dc787e634230552ab6bad
ernstt@umsystem.edu <ernstt@umsystem.edu>
2021-02-04 15:48:43 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: MayeCreate events
+ * Plugin URI: https://mayecreate.com/
+ * Description: This is the events post type plugin for MayeCreate Design
+ * Version: 1.0
+ * Author: Tyler Ernst, Rebecca Thomas, Creative Director Monica Pitts
+ * Author URI: http://www.mayecreate.com/
+ * License: GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
