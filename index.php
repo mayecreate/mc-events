274bf46afa26b77db7c830c1cff6e45f372985fc
ernstt@umsystem.edu <ernstt@umsystem.edu>
2021-02-04 15:55:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,4 @@
  * Version: 1.0
  * Author: Tyler Ernst, Rebecca Thomas, Creative Director Monica Pitts
  * Author URI: http://www.mayecreate.com/
- * License: GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
